52d408346fe06888af8fab82611c32daf1a31af9
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-10-06 16:46:19 +1300

    added more Talent data in the Talent Details page

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentDetail.js
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentDetail.js
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom';
 import TalentDetail from './TalentFeed/TalentDetail.jsx';
 
+
 ReactDOM.render(
     <TalentDetail />,
     document.getElementById('talent-detail-wrapper')
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed.js
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed.js
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom';
 import TalentFeed from './TalentFeed/TalentFeed.jsx';
 
+
 ReactDOM.render(
     <TalentFeed />,
     document.getElementById('talent-feed-wrapper')
